0b432174535d8a31e1e1493709a9c7ddeadce939
noormohammad20 <mdsaim204@gmail.com>
2022-02-15 01:40:57 +0600

    js added

--- js/logic.js
+++ js/logic.js
@@ -20,6 +20,8 @@ document.getElementById('click-btn').addEventListener('click', function () {
 })
 
 /* ৪. এখন ul এর নিচে একটা ইনপুট ফিল্ড আর একটা বাটন যোগ করো। ইনপুট ফিল্ড এ শুরুতে মান হবে শূন্য (০)। তারপর ইনপুট ফিল্ডের পাশের নতুন বাটন এ যতবার প্রেস করবে ততবার এক এক করে ইনপুট ফিল্ড এর মান এক এক করে বাড়বে।  */
+// . (চ্যালেঞ্জিং ওয়ান)উপরে বাটনে দুইটা শর্ত আছে। সেটা হচ্ছে সংখ্যা বাড়লেও সেটা হবে সর্বোচ্চ ৫। অর্থাৎ ০ থেকে এক এক করে বেড়ে বেড়ে পাঁচ পর্যন্ত হতে পারে। তার উপরে আর বাড়বে না। এবং ৫ হয়ে গেলে বাটনটা ডিজেবল হয়ে যাবে। 
+
 document.getElementById('btn').addEventListener('click', function increaseValue() {
     const input = document.getElementById('input');
     const inputNumber = input.value
@@ -33,6 +35,5 @@ document.getElementById('btn').addEventListener('click', function increaseValue(
 })
 
 
-// . (চ্যালেঞ্জিং ওয়ান)উপরে বাটনে দুইটা শর্ত আছে। সেটা হচ্ছে সংখ্যা বাড়লেও সেটা হবে সর্বোচ্চ ৫। অর্থাৎ ০ থেকে এক এক করে বেড়ে বেড়ে পাঁচ পর্যন্ত হতে পারে। তার উপরে আর বাড়বে না। এবং ৫ হয়ে গেলে বাটনটা ডিজেবল হয়ে যাবে। 
 
 
