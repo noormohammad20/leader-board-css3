328886f12c56c2f5825ce96655981c8f6bf7de7f
noormohammad20 <mdsaim204@gmail.com>
2022-02-15 01:39:15 +0600

    some js added

---
+++ js/logic.js
@@ -0,0 +1,38 @@
+/* . Top Players2022 নাম একটা লেখা আছে। অথবা Top Blogs লেখা আছে। এই দুইটাকে দুইটা ID দাও। HTML চেইঞ্জ করে ID সেট করো। তারপর জাভাস্ক্রিপ্ট দিয়েই সেই এলিমেন্ট খুঁজে বের করে তার টেক্সট এর কালার চেইঞ্জ করে ফেলো। */
+document.getElementById('top-players').style.color = 'goldenrod';
+document.getElementById('top-blogs').style.color = 'magenta'
+
+/* ২. জাভাস্ক্রিপ্ট দিয়ে player ক্লাস এর যতগুলা div আছে সবগুলাকে যেকোন একটা ব্যাকগ্রাউন্ড কালার দাও। তোমার ইচ্ছামতো যেকোন একটা ব্যাকগ্রাউন্ড কালার দিতে হবে। তবে কালারটা hex কালার না দিয়ে। rgba কালার দাও। যদি rgba বুঝতে না পারো। তাহলে গুগলে সার্চ দিয়ে একটা rgba কালার বের করে তারপর সেই কালার দাও।   */
+
+const players = document.getElementsByClassName('player');
+
+for (const player of players) {
+    player.style.backgroundColor = 'rgba(255, 99, 71, 1)';
+}
+
+/* ৩. ওয়েবসাইট এর নিচে একটা ul list যোগ করো। সেখানে তোমার ইচ্ছামতো তিনটা আইটেম যোগ করো। তারপর ul এর নিচে একটা বাটন যোগ করো। এখন তোমার কাজ হবে। বাটনে যতবার ক্লিক করবে। ul এর মধ্যে নতুন একটা লিস্ট আইটেম যোগ হবে।  */
+
+document.getElementById('click-btn').addEventListener('click', function () {
+    const ul = document.getElementById('ul')
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode('bootstrap'))
+    ul.appendChild(li)
+})
+
+/* ৪. এখন ul এর নিচে একটা ইনপুট ফিল্ড আর একটা বাটন যোগ করো। ইনপুট ফিল্ড এ শুরুতে মান হবে শূন্য (০)। তারপর ইনপুট ফিল্ডের পাশের নতুন বাটন এ যতবার প্রেস করবে ততবার এক এক করে ইনপুট ফিল্ড এর মান এক এক করে বাড়বে।  */
+document.getElementById('btn').addEventListener('click', function increaseValue() {
+    const input = document.getElementById('input');
+    const inputNumber = input.value
+    if (input.value < 5) {
+        input.value = parseInt(inputNumber) + 1
+
+    }
+    else {
+        document.getElementById('btn').setAttribute('disabled', true)
+    }
+})
+
+
+// . (চ্যালেঞ্জিং ওয়ান)উপরে বাটনে দুইটা শর্ত আছে। সেটা হচ্ছে সংখ্যা বাড়লেও সেটা হবে সর্বোচ্চ ৫। অর্থাৎ ০ থেকে এক এক করে বেড়ে বেড়ে পাঁচ পর্যন্ত হতে পারে। তার উপরে আর বাড়বে না। এবং ৫ হয়ে গেলে বাটনটা ডিজেবল হয়ে যাবে। 
+
+
